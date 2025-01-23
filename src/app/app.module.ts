2fa858afc6cd53b2da59f64ee6d4c6f42ebd2a45
Anderson Jimenez <andersonjesusujv@hotmail.com>
2025-01-23 10:10:50 -0500

    configuración de primeng con ejemplo de botón.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { themeConfig } from './config/theme/theme-config';
+
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    Ripple
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ themeConfig ],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
